af5d05614992ec52be9028f58b4b48dd4b7b1a81
Rasheed073 <rasheedphrasheed@gmail.com>
2023-03-23 23:40:15 +0530

    completed

--- app.js
+++ app.js
@@ -1,57 +1,63 @@
-const apikey = "3265874a2c77ae4a04bb96236a642d2f"
+const apiKey = "3265874a2c77ae4a04bb96236a642d2f";
 
 const main = document.getElementById("main")
 const formEl = document.getElementById("form")
 const search = document.getElementById("search")
 
 const url = (city) =>
-`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+    `https://api.openweathermap.org/data/2.5/
+weather?q=${city}&appid=${apiKey}`;
 
-async function getweatherByLocation(city){
+async function getWeatherByLocation(city){
  const resp = await fetch(url(city),{
-    orgin: "cors"});
-    const respData =await resp.json()
+    origin: "cors"});
+    const respData = await resp.json()
 
     addWeatherToPage(respData);
-}
-function addWeatherToPage(data){
-    const temp = Ktoc(data.main.temp)
 
-    const weather = document.createElement("div")
-    weather.classList.add("weather")
+}
+
+getWeatherByLocation("Cluj")
+
+
+function addWeatherToPage(data) {
+    
+    const temp = KtoC(data.main.temp);
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
 
     weather.innerHTML = `
-    <small>There are</small>
-    <h2>${temp}°C</h2>
-    <img src="https://openweathermap.org/img/w/ ${data.weather[0].icon}.png" />
-    `;
+    <h2><img src = "http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>${temp}°C 
+    <img src = "http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>
+    </h2>
+
+    <small>${data.weather[0].main} </small>`;
 
     // cleanup
 
     main.innerHTML = "";  
 
-    console.log(main);
-
-    main.appenChaild(weather);
+  
+    main.appendChild(weather); 
     
+} 
+
+function KtoC(K){formEl
+return Math.floor (K- 273.15)
 }
 
-function Ktoc(k){formEl
-    return (k - 273.15).toFixed(2)
-}
 
+formEl.addEventListener('submit', (e) => {
 
-formEl.addEventListener('submit', (event) => {
-
-    event.preventDefault();
+    e.preventDefault();
 
     const city = search.value;
 
-    if(city) {
-        getweatherByLocation(city)
+    if (city) {
+        getWeatherByLocation(city)
     }
 
-})
+});
 
 
 
